9aee91fee8f8eb3a3f5f2d35739526a6a98df9f6
santhosh <santhosh.s31999@gmail.com>
2020-10-15 09:59:48 +0530

    17 committs
    17 files committed

---
+++ connect.php
@@ -0,0 +1,25 @@
+<?php
+$servername = "localhost";
+$username = "username";
+$password = "";
+$dbname = "myDB";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql = "INSERT INTO MyGuests (firstname, lastname, email,password,conf.pwd,gender)
+VALUES ('John', 'Doe', 'john@example.com','santhosh123','santhosh123','male')";
+
+if (mysqli_query($conn, $sql)) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+?>
+
